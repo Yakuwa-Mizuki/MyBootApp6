37d7bd153cd0c4365f6d629a9875045390081110
Yakuwa-Mizuki <tejc210826@gmail.com>
2024-06-04 10:32:58 +0900

    課題08（完成）

--- src/main/java/jp/te4a/spring/boot/myapp8/mybootapp8/BookBean.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/mybootapp8/BookBean.java
@@ -1,13 +1,15 @@
-package jp.te4a.spring.boot.myapp7.mybootapp7;
+package jp.te4a.spring.boot.myapp8.mybootapp8;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookBean {
     Integer id;
     String title;
